01990581f9833956abb6b665b9dfdd13154dc7cf
araemon <araemonz@gmail.com>
2018-12-11 18:25:51 +0900

    update

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+//  header('Location: http://192.168.100.101/blog/', true, 301);
+//  exit;
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+<title>101010.fun</title>
+<meta name="viewport" content="width=device-width,initial-scale=1">
+<style>
+
+
+</style>
+</head>
+<body>
+101010.fun
+</body>
+</html>
